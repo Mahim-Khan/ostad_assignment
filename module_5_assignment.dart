277313b5419341ef990c7eb59149a23ce2086d7d
Abdullah Al Fahim <60973245+Mahim-Khan@users.noreply.github.com>
2025-06-25 21:50:14 +0600

    Update module_5_assignment.dart

--- module_5_assignment.dart
+++ module_5_assignment.dart
@@ -1,1 +1,25 @@
 
+abstract class Vehicle {
+  int _speed = 0;
+
+  void move();
+
+  void setSpeed(int speed) {
+    _speed = speed;
+  }
+
+  int get speed => _speed;
+}
+
+class Car extends Vehicle {
+  @override
+  void move() {
+    print('The car is moving at $speed km/h');
+  }
+}
+
+void main() {
+  Car myCar = Car();
+  myCar.setSpeed(80); // set speed using the setter
+  myCar.move();       // prints: The car is moving at 80 km/h
+}
